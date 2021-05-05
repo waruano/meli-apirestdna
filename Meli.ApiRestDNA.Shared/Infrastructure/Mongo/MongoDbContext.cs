3a867847de3b47f0e140bf18a70d929a20b26b30
Alexis Ruano <waruano9212@gmail.com>
2021-05-05 00:04:02 -0500

    relocation files

--- Meli.ApiRestDNA.Shared/Infrastructure/Mongo/MongoDbContext.cs
+++ Meli.ApiRestDNA.Shared/Infrastructure/Mongo/MongoDbContext.cs
@@ -1,10 +1,8 @@
 ﻿using MongoDB.Driver;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using MongoDB.Bson;
 
 namespace Meli.ApiRestDNA.Shared.Infrastructure.Mongo
 {
@@ -27,78 +25,15 @@ namespace Meli.ApiRestDNA.Shared.Infrastructure.Mongo
                 .FirstOrDefault())?.CollectionName;
         }
 
-        //public virtual IQueryable<TDocument> AsQueryable()
-        //{
-        //    return _collection.AsQueryable();
-        //}
-
-        //public virtual IEnumerable<TDocument> FilterBy(
-        //    Expression<Func<TDocument, bool>> filterExpression)
-        //{
-        //    return _collection.Find(filterExpression).ToEnumerable();
-        //}
-
-        //public virtual IEnumerable<TProjected> FilterBy<TProjected>(
-        //    Expression<Func<TDocument, bool>> filterExpression,
-        //    Expression<Func<TDocument, TProjected>> projectionExpression)
-        //{
-        //    return _collection.Find(filterExpression).Project(projectionExpression).ToEnumerable();
-        //}
-
-        //public virtual TDocument FindOne(Expression<Func<TDocument, bool>> filterExpression)
-        //{
-        //    return _collection.Find(filterExpression).FirstOrDefault();
-        //}
-
         public virtual Task<TDocument> FindOneAsync(Expression<Func<TDocument, bool>> filterExpression)
         {
             return Task.Run(() => _collection.Find(filterExpression).FirstOrDefaultAsync());
         }
 
-        //public virtual TDocument FindById(string id)
-        //{
-        //    var objectId = new ObjectId(id);
-        //    var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, objectId);
-        //    return _collection.Find(filter).SingleOrDefault();
-        //}
-
-        //public virtual Task<TDocument> FindByIdAsync(string id)
-        //{
-        //    return Task.Run(() =>
-        //    {
-        //        var objectId = new ObjectId(id);
-        //        var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, objectId);
-        //        return _collection.Find(filter).SingleOrDefaultAsync();
-        //    });
-        //}
-
-
-        //public virtual void InsertOne(TDocument document)
-        //{
-        //    _collection.InsertOne(document);
-        //}
-
         public virtual Task InsertOneAsync(TDocument document)
         {
             return Task.Run((Func<Task?>)(() => _collection.InsertOneAsync(document)));
         }
-
-        //public void InsertMany(ICollection<TDocument> documents)
-        //{
-        //    _collection.InsertMany(documents);
-        //}
-
-
-        //public virtual async Task InsertManyAsync(ICollection<TDocument> documents)
-        //{
-        //    await _collection.InsertManyAsync(documents);
-        //}
-
-        //public void ReplaceOne(TDocument document)
-        //{
-        //    var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, document.Id);
-        //    _collection.FindOneAndReplace(filter, document);
-        //}
 
         public virtual async Task ReplaceOneAsync(TDocument document)
         {
@@ -106,41 +41,5 @@ namespace Meli.ApiRestDNA.Shared.Infrastructure.Mongo
             await _collection.FindOneAndReplaceAsync(filter, document);
         }
 
-        //public void DeleteOne(Expression<Func<TDocument, bool>> filterExpression)
-        //{
-        //    _collection.FindOneAndDelete(filterExpression);
-        //}
-
-        //public Task DeleteOneAsync(Expression<Func<TDocument, bool>> filterExpression)
-        //{
-        //    return Task.Run(() => _collection.FindOneAndDeleteAsync(filterExpression));
-        //}
-
-        //public void DeleteById(string id)
-        //{
-        //    var objectId = new ObjectId(id);
-        //    var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, objectId);
-        //    _collection.FindOneAndDelete(filter);
-        //}
-
-        //public Task DeleteByIdAsync(string id)
-        //{
-        //    return Task.Run(() =>
-        //    {
-        //        var objectId = new ObjectId(id);
-        //        var filter = Builders<TDocument>.Filter.Eq(doc => doc.Id, objectId);
-        //        _collection.FindOneAndDeleteAsync(filter);
-        //    });
-        //}
-
-        //public void DeleteMany(Expression<Func<TDocument, bool>> filterExpression)
-        //{
-        //    _collection.DeleteMany(filterExpression);
-        //}
-
-        //public Task DeleteManyAsync(Expression<Func<TDocument, bool>> filterExpression)
-        //{
-        //    return Task.Run(() => _collection.DeleteManyAsync(filterExpression));
-        //}
     }
 }
